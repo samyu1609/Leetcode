d53a5908229066d4ee1a55861d3b51921bd37c4e
Samyuktha <samyukthamn16@gmail.com>
2026-04-15 16:46:14 +0530

    Time: 14 ms (8.03%) | Memory: 52.1 MB (21.63%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+
+                if (num == '.') continue;
+                String row = "row" + i + num;
+                String col = "col" + j + num;
+                String box = "box" + (i/3) + (j/3) + num;
+                if (set.contains(row) || set.contains(col) || set.contains(box)) {
+                    return false;
+                }
+                set.add(row);
+                set.add(col);
+                set.add(box);
+            }
+        }
+        return true;
+    }
+}
